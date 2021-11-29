1af36c2716d800504a7c8df8918817ae45981495
Kuzil-Taras <olya.velychenko@gmail.com>
2021-11-29 14:40:24 +0000

    add burger menu

--- js/buger-menu.js
+++ js/buger-menu.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$('.header__burger').click(function (event) {
+	$('.header__burger').click(function (_event) {
 		$('.header__burger,.menu-header').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
