b21b64cf79182a4e3526f78561d56195627db820
Kuzil-Taras <olya.velychenko@gmail.com>
2021-11-29 17:13:28 +0000

    Corrected burger-menu

--- js/buger-menu.js
+++ js/buger-menu.js
@@ -1,6 +1,20 @@
-$(document).ready(function() {
-	$('.header__burger').click(function (_event) {
-		$('.header__burger,.menu-header').toggleClass('active');
-		$('body').toggleClass('lock');
-	});
-});
+(function() {
+    const iconBurger = document.querySelector(".header__burger");
+    const navigationsBody = document.querySelector(".menu-header");
+    iconBurger.addEventListener("click", function(e) {
+        document.body.classList.toggle("lock");
+        iconBurger.classList.toggle("active");
+        navigationsBody.classList.toggle("active");
+    });
+
+    const navLinks = document.querySelectorAll(".menu-header-elements");
+    navLinks.forEach((navLink) => {
+        navLink.addEventListener("click", (_) => {
+            if (iconBurger.classList.contains("active")) {
+                document.body.classList.remove("lock");
+                iconBurger.classList.remove("active");
+                navigationsBody.classList.remove("active");
+            }
+        });
+    });
+})();
